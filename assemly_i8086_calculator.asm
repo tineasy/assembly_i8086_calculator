c1ad66f63206e220e124c5cc115662f52cde71b0
Valentin <valentin.stets@icloud.com>
2021-01-24 20:29:13 +0100

    Refactored code, so it can be readable

--- assemly_i8086_calculator.asm
+++ assemly_i8086_calculator.asm
@@ -1,23 +1,24 @@
 org 100h
 
 .data
-stra db "a", 32, "=", 32, "$"
-strb db 10, 13, "b", 32, "=", 32, "$"
-stro db 10, 13, "Operation?", 32, "(+,", 32, "-,", 32, "*,", 32, "/):", 32, "$"
-strr db 10, 13, "Result:", 32, "$"
-stre db 10, 13, "Wrong", 32, "input!", 32, "Try", 32, "again.", 10, 13, "$"
-strz db 10, 13, "b", 32, "cannot", 32, "be", 32, "equal", 32, "to", 32, "zero!", 32, "Try", 32, "again.", 10, 13, "$"
-strk db 10, 13, 10, 13, "Press", 32, "any", 32, "key", 32, "to", 32, "exit.$"
-ostatak dw ?
+string_a db "a = ", "$"
+string_b db 10, 13, "b = ", "$"
+string_operation db 10, 13, "Operation? (+, -, *, /): ", "$"
+string_result db 10, 13, "Result: ", "$"
+string_error db 10, 13, "Wrong input", 33, " Try again.", 10, 13, "$"
+string_zero db 10, 13, "b cannot be equal to zero", 32, " Try again.", 10, 13, "$"
+string_key db 10, 13, 10, 13, "Press any key to exit. ", "$"
+remainder_of_division dw ?
 b dw ?
 
 .code
-; unos a
+; printing out string_a
 mov ah, 9
-lea dx, stra
+lea dx, string_a
 int 21h
 
-mov ah, 1
+; getting input of two-digit a
+mov ah, 1  ; first digit
 int 21h
 sub al, 48
 mov bl, 10
@@ -25,176 +26,180 @@ mul bl
 xor ah, ah
 mov bx, ax
 
-mov ah, 1
+mov ah, 1  ; second digit
 int 21h
 sub al, 48
 xor ah, ah
-add bx, ax
-; bx = a
-jmp input_b
+
+add bx, ax  ; a is located in bx
+
+jmp input_b  ; jumping over zero string, because there is no selected operation yet
 
 zero:
-mov ah, 9
-lea dx, strz
-int 21h
+    mov ah, 9
+    lea dx, string_zero
+    int 21h
 
 input_b:
-; unos b
-mov ah, 9
-lea dx, strb
-int 21h
+    ; getting input of two-digit b
+    mov ah, 9
+    lea dx, string_b
+    int 21h
 
-mov ah, 1
-int 21h
-sub al, 48
-mov cl, 10
-mul cl
-xor ah, ah
-mov cx, ax
+    mov ah, 1  ; first digit
+    int 21h
+    sub al, 48
+    mov cl, 10
+    mul cl
+    xor ah, ah
+    mov cx, ax
 
-mov ah, 1
-int 21h
-sub al, 48
-xor ah, ah
-add cx, ax
-mov b, cx
-; cx = b
-jmp operation
+    mov ah, 1  ; second digit
+    int 21h
+    sub al, 48
+    xor ah, ah
+    add cx, ax
+
+    mov b, cx  ; b is located in cx
+
+    jmp operation  ; jumping over error string
 
 wrong_input:
-mov ah, 9
-lea dx, stre
-int 21h
+    mov ah, 9
+    lea dx, string_error
+    int 21h
 
 operation:
-mov ah, 9
-lea dx, stro
-int 21h
+    mov ah, 9
+    lea dx, string_operation
+    int 21h
 
-mov ah, 1
-int 21h
+    mov ah, 1
+    int 21h
 
-cmp al, "+"
-jz plus
-cmp al, "-"
-jz minus
-cmp al, "*"
-jz multiply
-cmp al, "/"
-jz divide
-jmp wrong_input
+    cmp al, "+"
+    jz plus
+    cmp al, "-"
+    jz minus
+    cmp al, "*"
+    jz multiply
+    cmp al, "/"
+    jz divide
+    jmp wrong_input
 
 plus:
-add bx, cx
-jmp output
+    add bx, cx
+    jmp output  ; jumping over to output
 
 minus:
-sub bx, cx
-jmp output
+    sub bx, cx
+    jmp output  ; jumping over to output
 
 multiply:
-mov ax, bx
-mul cx
-mov bx, ax
-jmp output
+    mov ax, bx
+    mul cx
+    mov bx, ax
+    jmp output  ; jumping over to output
 
 divide:
-cmp cx, 0
-jz zero
-mov ax, bx
-xor dx, dx
-div cx
-mov ostatak, dx
-mov bx, ax
+    cmp cx, 0
+    jz zero
+    mov ax, bx
+    xor dx, dx
+    div cx
+    mov remainder_of_division, dx
+    mov bx, ax
+    ; no jump because output goes next anyways
 
 output:
-mov ah, 9
-lea dx, strr
-int 21h
+    mov ah, 9
+    lea dx, string_result
+    int 21h
 
-cmp bx, 0
-jge not_minus
+    cmp bx, 0
+    jge not_minus
 
-mov ah, 2
-mov dx, 45
-int 21h
-neg bx
+    mov ah, 2
+    mov dx, 45
+    int 21h
+    neg bx
 
 not_minus:
-mov ax, bx
-mov bx, 1000
-xor dx, dx
-div bx
-mov cx, dx
-add ax, 48
-mov dx, ax
-mov ah, 2
-int 21h
-xor ax, ax
+    ; printing out four-digit number
+    mov ax, bx
+    mov bx, 1000
+    xor dx, dx
+    div bx
+    mov cx, dx
+    add ax, 48
+    mov dx, ax
+    mov ah, 2
+    int 21h
+    xor ax, ax
 
-mov ax, cx
-mov bx, 100
-xor dx, dx
-div bx
-mov cx, dx
-add ax, 48
-mov dx, ax
-mov ah, 2
-int 21h
-xor ax, ax
+    mov ax, cx
+    mov bx, 100
+    xor dx, dx
+    div bx
+    mov cx, dx
+    add ax, 48
+    mov dx, ax
+    mov ah, 2
+    int 21h
+    xor ax, ax
 
-mov ax, cx
-mov bx, 10
-xor dx, dx
-div bx
-mov cx, dx
-add ax, 48
-mov dx, ax
-mov ah, 2
-int 21h
-xor ax, ax
+    mov ax, cx
+    mov bx, 10
+    xor dx, dx
+    div bx
+    mov cx, dx
+    add ax, 48
+    mov dx, ax
+    mov ah, 2
+    int 21h
+    xor ax, ax
 
-add cx, 48
-mov dx, cx
-mov ah, 2
-int 21h
+    add cx, 48
+    mov dx, cx
+    mov ah, 2
+    int 21h
 
-; ostatak
-mov dx, 46
-mov ah, 2
-int 21h
-xor ax, ax
+    ; printing out . and remainder of the division
+    mov dx, 46
+    mov ah, 2
+    int 21h
+    xor ax, ax
 
-mov ax, ostatak
-mov bx, 10
-mul bx
-xor bx, bx
-mov bx, b
-xor dx, dx
-div bx
-mov cx, dx
-mov dx, ax
-add dx, 48
-mov ah, 2
-int 21h
+    mov ax, remainder_of_division
+    mov bx, 10
+    mul bx
+    xor bx, bx
+    mov bx, b
+    xor dx, dx
+    div bx
+    mov cx, dx
+    mov dx, ax
+    add dx, 48
+    mov ah, 2
+    int 21h
 
-mov ax, cx
-mov bx, 10
-mul bx
-xor bx, bx
-mov bx, b
-xor dx, dx
-div bx
-mov dx, ax
-add dx, 48
-mov ah, 2
-int 21h
+    mov ax, cx
+    mov bx, 10
+    mul bx
+    xor bx, bx
+    mov bx, b
+    xor dx, dx
+    div bx
+    mov dx, ax
+    add dx, 48
+    mov ah, 2
+    int 21h
 
-; finish
-mov ah, 9
-lea dx, strk
-int 21h
-mov ah, 0
-int 16h
+    mov ah, 9
+    lea dx, string_key
+    int 21h
+    mov ah, 0
+    int 16h
 
 ret
+
